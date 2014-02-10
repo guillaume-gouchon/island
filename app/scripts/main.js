25756b9e8b08d899c23f38c3a94d7cc857154221
guillaume <guillaume@ubuntu.(none)>
2014-02-10 17:36:54 +0000

    implements html overlays

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,103 +1,17 @@
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, loader;
 var controls, time = Date.now();
 var skybox;
 var ray;
-
-var assetsPath = 'images/'
-
-function requestFullscreen () {
-	var element = document.body;
-
-	var fullscreenchange = function ( event ) {
-		if ( document.fullscreenElement === element || document.mozFullscreenElement === element 
-			|| document.mozFullScreenElement === element ) {
-			document.removeEventListener( 'fullscreenchange', fullscreenchange );
-		document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
-		element.requestPointerLock();
-	}
-}
-
-document.addEventListener( 'fullscreenchange', fullscreenchange, false );
-document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
-
-element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
-element.requestFullscreen();
-}
-
-function initBrowser () {
-	var blocker = document.getElementById( 'blocker' );
-
-	var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
-	if ( havePointerLock ) {
-
-		var element = document.body;
-
-		var pointerlockchange = function ( event ) {
-
-			if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
-
-				controls.enabled = true;
-
-				blocker.style.display = 'none';
-
-			} else {
-
-				controls.enabled = false;
-
-				blocker.style.display = '-webkit-box';
-				blocker.style.display = '-moz-box';
-				blocker.style.display = 'box';
-
-			}
-
-		}
-
-		var pointerlockerror = function ( event ) {
-			console.log("Error while locking pointer")
-		}
-
-		// Hook pointer lock state change events
-		document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
-
-		document.addEventListener( 'pointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
-
-		blocker.addEventListener( 'click', function ( event ) {
-
-			blocker.style.display = 'none';
-
-			// Ask the browser to lock the pointer
-			element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
-
-			if ( /Firefox/i.test( navigator.userAgent ) ) {
-
-				requestFullscreen();
-				element.requestPointerLock();
-
-			} else {
-				requestFullscreen();
-				element.requestPointerLock();
-
-			}
-
-		}, false );
-
-	} else {
-
-		blocker.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
-
-	}
-}
+var models = {};
+var objects = [];
+var assetsPath = 'assets/'
 
 
 function initScene() {
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100000 );
-	camera.position.y = 80;
+	// camera.position.y = 500;
 
 	scene = new THREE.Scene();
 	// scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
@@ -127,9 +41,14 @@ function initScene() {
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
+	loader = new THREE.JSONLoader();
+
 	addSkybox();
 
 	initHeightMap();
+	initSkyboxHeightMap();
+
+	loadAssets();
 
 }
 
@@ -146,24 +65,24 @@ function loop() {
 
 	requestAnimationFrame( loop );
 
-	// controls.isOnObject( false );
+	controls.isOnObject( false );
 
-	// ray.ray.origin.copy( controls.getObject().position );
-	// ray.ray.origin.y -= 10;
+	ray.ray.origin.copy( controls.getObject().position );
+	ray.ray.origin.y -= 10;
 
-	// var intersections = ray.intersectObjects( objects );
+	var intersections = ray.intersectObjects( objects );
 
-	// if ( intersections.length > 0 ) {
+	if ( intersections.length > 0 ) {
 
-	// 	var distance = intersections[ 0 ].distance;
+		var distance = intersections[ 0 ].distance;
 
-	// 	if ( distance > 0 && distance < 10 ) {
+		if ( distance > 0 && distance < 10 ) {
 
-	// 		controls.isOnObject( true );
+			controls.isOnObject( true );
 
-	// 	}
+		}
 
-	// }
+	}
 
 	controls.update( Date.now() - time );
 
@@ -172,7 +91,7 @@ function loop() {
 	time = Date.now();
 
 	// rotate sky
-	skybox.rotation.y+= de2ra(0.002);
+	skybox.rotation.y+= de2ra(0.005);
 }
 
 
@@ -217,7 +136,7 @@ function initHeightMap () {
 	var bumpTexture = new THREE.ImageUtils.loadTexture( assetsPath + 'heightmap.png' );
 	bumpTexture.wrapS = bumpTexture.wrapT = THREE.RepeatWrapping; 
 	// magnitude of normal displacement
-	var bumpScale = 200.0;
+	var bumpScale = 300.0;
 
 	var sandyTexture = new THREE.ImageUtils.loadTexture( assetsPath + 'soil_gravel.png' );
 	sandyTexture.wrapS = sandyTexture.wrapT = THREE.RepeatWrapping; 
@@ -246,7 +165,7 @@ function initHeightMap () {
 	}   
 	);
 
-	var landGeometry = new THREE.PlaneGeometry( 2000, 2000, 50, 50);
+	var landGeometry = new THREE.PlaneGeometry( 5000, 5000, 256, 256);
 	landGeometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 	landGeometry.computeFaceNormals();
 	landGeometry.computeVertexNormals();
@@ -255,8 +174,76 @@ function initHeightMap () {
 	scene.add( planeSurface );
 }
 
+function initSkyboxHeightMap () {
+	var bumpTexture = new THREE.ImageUtils.loadTexture( assetsPath + 'skybox_heightmap.png' );
+	bumpTexture.wrapS = bumpTexture.wrapT = THREE.RepeatWrapping; 
+	// magnitude of normal displacement
+	var bumpScale = 4000.0;
+
+	var sandyTexture = new THREE.ImageUtils.loadTexture( assetsPath + 'soil_gravel.png' );
+	sandyTexture.wrapS = sandyTexture.wrapT = THREE.RepeatWrapping; 
+	
+	var grassTexture = new THREE.ImageUtils.loadTexture( assetsPath + 'moss_gravel_dense.png' );
+	grassTexture.wrapS = grassTexture.wrapT = THREE.RepeatWrapping; 
+
+	var rockyTexture = new THREE.ImageUtils.loadTexture( assetsPath + 'rock_rough_mossy.png' );
+	rockyTexture.wrapS = rockyTexture.wrapT = THREE.RepeatWrapping; 
+
+	var customUniforms = {
+		bumpScale: 		{ type: "f", value: bumpScale },
+		bumpTexture:	{ type: "t", value: bumpTexture },
+		sandyTexture:	{ type: "t", value: sandyTexture },
+		grassTexture:	{ type: "t", value: grassTexture },
+		rockyTexture:	{ type: "t", value: rockyTexture },
+	};
+
+	// create custom material from the shader code above
+	//   that is within specially labelled script tags
+	var customMaterial = new THREE.ShaderMaterial( 
+	{
+		uniforms: customUniforms,
+		vertexShader:   document.getElementById('vertexShader').textContent,
+		fragmentShader: document.getElementById('fragmentShader').textContent,
+	}   
+	);
+
+	var landGeometry = new THREE.PlaneGeometry( 10000, 10000, 256, 256);
+	landGeometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+	var skyboxMountains = new THREE.Mesh(landGeometry, customMaterial);
+	skyboxMountains.position.y = -2000;
+	scene.add( skyboxMountains );
+}
 
 
-initBrowser();
-initScene();
-loop();
+function loadAssets() {
+	this.loader.load(assetsPath + 'cedar.js', geometryLoaded());
+}
+
+
+/**
+*	Callback when a geometry is loaded.
+*/
+function geometryLoaded () {
+	return function (geometry, materials) {
+		models.swamptree = { geometry: geometry, materials: materials };
+		addRandomTrees();
+	};
+}
+
+function addRandomTrees () {
+	for (var i = 0; i < 1; i++) {
+		var tree = new THREE.Mesh(models.swamptree.geometry, new THREE.MeshFaceMaterial(models.swamptree.materials));
+		tree.position = {
+			x: 0,
+			y: 500,
+			z: 0
+		}
+		scene.add(tree);
+	}
+}
+
+
+
+// initBrowser();
+// initScene();
+// loop();
---
+++ app/scripts/overlay.js
@@ -0,0 +1,111 @@
+
+/**
+*	CLICK TO START
+*/
+var clickZone = document.getElementById('clickZone');
+
+function start () {
+	clickZone.removeEventListener('click', start);
+
+	// hide blocker, then remove it
+	var blocker = document.getElementById('blocker');
+	blocker.classList.add('hide');
+	setTimeout(function () {
+		document.body.removeChild(blocker);
+
+		// request browser's fullscreen and pointer lock
+		requestFullscreen();
+		requestPointerLock();
+	}, 500);
+
+}
+
+
+/**
+*	FULLSCREEN
+*/
+function requestFullscreen () {
+	var element = document.body;
+
+	var fullscreenchange = function ( event ) {
+		if ( document.fullscreenElement === element || document.mozFullscreenElement === element 
+			|| document.mozFullScreenElement === element ) {
+			document.removeEventListener( 'fullscreenchange', fullscreenchange );
+			document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+		}
+	}
+
+	document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+	document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+	// Ask the browser for fullscreen mode
+	element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+	element.requestFullscreen();
+}
+
+
+/**
+*	POINTER LOCK
+*/
+function requestPointerLock () {
+	var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document 
+							|| 'webkitPointerLockElement' in document;
+	if (havePointerLock) {
+		var element = document.body;
+
+		var pointerlockchange = function (event) {
+			if ( document.pointerLockElement === element || document.mozPointerLockElement === element 
+				|| document.webkitPointerLockElement === element ) {
+				// controls.enabled = true;
+			} else {
+				// controls.enabled = false;
+			}
+		}
+
+		document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+		document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+		document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+	
+		// Ask the browser to lock the pointer
+		element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+		element.requestPointerLock();
+	}
+}
+
+
+/**
+*	CREDITS
+*/
+var credits = document.getElementById('credits');
+var creditsLink = credits.getElementsByClassName('link')[0];
+
+function showCredits () {
+	credits.getElementsByClassName('content')[0].classList.remove('hide');
+	credits.removeEventListener('click', showCredits);
+}
+
+creditsLink.addEventListener('click', showCredits, false);
+
+
+/**
+*	WEBGL ERROR
+*/
+function isWebGLEnabled() {
+	try { 
+		return !! window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl');
+	} catch(e) { 
+		return false; 
+	}
+}
+
+
+/**
+*	CHECK IF WEBGL IS ENABLED
+*/
+if (isWebGLEnabled()) {
+	clickZone.addEventListener('click', start, false);
+} else {
+	document.getElementById('webGLError').classList.remove('hide');
+	var instructions = document.getElementById('instructions');
+	instructions.style.opacity = 0.1;
+}
