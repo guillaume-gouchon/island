3aa1b7192974ded88ae3c5176290b0c382483cf0
guillaume <guillaume@ubuntu.(none)>
2014-02-19 11:04:34 +0000

    u

--- app/scripts/sound.js
+++ app/scripts/sound.js
@@ -1,23 +1,27 @@
 var SeaSound = function ( soundFilePath, radius, volume ) {
 
-	var audio = document.createElement( 'audio' );
+	var audio1 = createAudioElement();
+	var audio2 = createAudioElement();
+	
+	audio1.addEventListener('timeupdate', function () {
+		var remain = this.duration - this.currentTime;
+		console.log(remain)
+        if (remain <= 0.35){
+            audio2.currentTime = 0;
+		    audio2.play();
+        }
+	}, false);
 
-	var source = document.createElement( 'source' );
-	source.src = soundFilePath + '.mp3';
-	audio.appendChild(source);
-
-	source = document.createElement( 'source' );
-	source.src = soundFilePath + '.ogg';
-	audio.appendChild(source);
-
-	audio.loop = true;
-	audio.addEventListener('ended', function () {
-	    this.currentTime = 0;
-	    this.play();
+	audio2.addEventListener('timeupdate', function () {
+		var remain = this.duration - this.currentTime;
+        if (remain <= 0.35){
+            audio1.currentTime = 0;
+		    audio1.play();
+        }
 	}, false);
 
 	this.play = function () {
-		audio.play();
+		audio1.play();
 	}
 
 	var centerPoint = new THREE.Particle();
@@ -26,18 +30,31 @@ var SeaSound = function ( soundFilePath, radius, volume ) {
 
 	this.update = function ( camera ) {
 
+		// increase volume the more we are closer to the sea
 		var distance = centerPoint.position.distanceTo( camera.position );
-
 		if ( distance <= radius ) {
-
-			audio.volume = Math.max(0, volume * distance / radius);
-
+			audio1.volume = Math.max(0, volume * distance / radius);
+			audio2.volume = Math.max(0, volume * distance / radius);
 		} else {
-
-			audio.volume = 0;
-
+			audio1.volume = 0;
+			audio2.volume = 0;
 		}
 
+	}
+
+	function createAudioElement() {
+		var audio = document.createElement('audio');
+		audio.preload = true;
+
+		var source = document.createElement( 'source' );
+		source.src = soundFilePath + '.mp3';
+		audio.appendChild(source);
+
+		source = document.createElement( 'source' );
+		source.src = soundFilePath + '.ogg';
+		audio.appendChild(source);
+
+		return audio;
 	}
 
 }
